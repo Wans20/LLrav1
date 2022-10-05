957743989a6144bc2b7755d1e9d4b03639809cf0
Wans20n <wans200102@gmail.com>
2022-10-05 19:40:09 +0700

    Set Up Seeder and Faker

--- database/seeders/MajorSeeder.php
+++ database/seeders/MajorSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Major;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class MajorSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class MajorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // menghapus semua data di database
+        Major::truncate();
+        $majors=["ASE","DBM","CDM","OAA"];
+        foreach( $majors as $key => $major){
+            Major::create([
+                "name" => $major,
+                "description" => "Ini jurusan $major",
+            ]);
+        }
+
+        // Major::create([
+        //     "name" => "Sastra",
+        //     "description" => "Buat Buku"
+        // ]);
     }
 }
--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory;
+use App\Models\Major;
+use App\Models\Student;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class StudentSeeder extends Seeder
 {
@@ -14,6 +17,29 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
+            // menghapus semua data di database
+            Student::truncate();
+            // local factory in indonesia
+            $faker = Factory::create('id_ID');
+            // panggil semua jurusan
+            $majors = Major::get();
+            for ($i=0; $i < 100; $i++){
+                $gender = ($i % 2)?"Male":"Female";
+                Student::create([
+                    'name' => $faker->name($gender),
+                    'date_birth' => "1945-07-17",
+                    'gender' => "Male",
+                    'address' => $faker->address,
+                    'major_id' => $majors->random()->id
+                ]);
+            }
+        //    Student::create([
+        //     "name" => "Adit",
+        //     "date_birth" => "1927-07-01",
+        //     "gender" => "Male",
+        //     "address" => "Jl.Maju Mundur",
+        //     "major_id" => 1
+        //    ]);
+        
     }
 }
